41f02188fd7afa20f0bc348f8d376d2985f219d2
suryyyansh <118013430+suryyyansh@users.noreply.github.com>
2025-08-14 23:22:09 +0530

    Sidebar main functionality added

--- utils.ts
+++ utils.ts
@@ -1,3 +1,23 @@
-export getValidPaths(path) = (path) => {
+import fs from 'fs';
+import path from 'path';
 
-}
+export type FileTree = {
+  [key: string]: null | FileTree
+};
+
+export const readAllFiles = (dir: string) => {
+  const files = fs.readdirSync(dir, { withFileTypes: true });
+
+  let output: FileTree = {};
+
+  for (const file of files) {
+    if (file.isDirectory()) {
+      const fullDirPath = path.join(dir, file.name);
+      output[file.name] = readAllFiles(fullDirPath);
+    } else {
+      output[file.name] = null;
+    }
+  }
+
+  return output;
+};
